53279c867db41e56353bc39b3359debbeb2bbe36
Ahrielyoung <ahrielyoung19@gmail.com>
2024-12-06 21:04:09 -0500

    Update AI.js

--- AI.js
+++ AI.js
@@ -16,52 +16,51 @@
 //all info you want to include
 
 var data = {
-  chatinit:{
-    title: ["Hello <span class='emoji'> &#128075;</span", "I am Mr. AY", "How can I help you?"],
-    options: ["Math" , "Code", "TechNews","Random" ]
+  chatinit: {
+    title: ["Hello <span class='emoji'> &#128075;</span>", "I am Mr. AY", "How can I help you?"],
+    options: ["Math", "Code", "TechNews", "Random"]
   },
   math: {
-    title:["Please select a category"],
-    options:['Addition','Subtraction','Multiplcation','Division'],
-    url : {
-      link:["https://www.mathway.com/Algebra", "https://www.mathway.com/Algebra", "https://www.mathway.com/Algebra", "https://www.mathway.com/Algebra"]
+    title: ["Please select a category"],
+    options: ['Addition', 'Subtraction', 'Multiplication', 'Division'],
+    url: {
+      link: ["https://www.mathway.com/Algebra", "https://www.mathway.com/Algebra", "https://www.mathway.com/Algebra", "https://www.mathway.com/Algebra"]
     }
   },
-  code:{
-      title: ["Please select a language"],
-      options: ['HTML','Python', 'Java'],
-      url: {
-          link:["https://workik.com/ai-code-generator"]
-      }
+  code: {
+    title: ["Please select a language"],
+    options: ['HTML', 'Python', 'Java'],
+    url: {
+      link: ["https://workik.com/ai-code-generator"]
+    }
   },
-  technews:{
-      title:["Top 5 headlines in the Technology World"],
-      options:['Big Tech says Austrilia rushed social media ban for youths under 16', 'TSMC founder Morris Chang offered top job to Jensen Huang memoir shows', 'Meta faces trial in October on unfair competition case lodged by Spanish media', 'Canadian news compainies challenge OpenAI alleged copyright breaches','Australian social media ban started with call to act by politicians wife'],
-      url:{
-          link:["https://www.reuters.com/technology/big-tech-says-australia-rushed-social-media-ban-youths-under-16-2024-11-29/","https://www.reuters.com/technology/tsmc-founder-morris-chang-offered-top-job-jensen-huang-memoir-shows-2024-11-29/", "https://www.reuters.com/technology/meta-faces-trial-october-unfair-competition-case-lodged-by-spanish-media-2024-11-29/", "https://www.reuters.com/sustainability/boards-policy-regulation/major-canadian-news-media-companies-launch-legal-action-against-openai-2024-11-29/", "https://www.reuters.com/world/asia-pacific/australian-social-media-ban-started-with-call-act-by-poiliticans-wife-2024-11-29/" ]
+  technews: {
+    title: ["Top 5 headlines in the Technology World"],
+    options: ['Big Tech says Australia rushed social media ban for youths under 16', 'TSMC founder Morris Chang offered top job to Jensen Huang memoir shows', 'Meta faces trial in October on unfair competition case lodged by Spanish media', 'Canadian news companies challenge OpenAI alleged copyright breaches', 'Australian social media ban started with call to act by politicians wife'],
+    url: {
+      link: [
+        "https://www.reuters.com/technology/big-tech-says-australia-rushed-social-media-ban-youths-under-16-2024-11-29/",
+        "https://www.reuters.com/technology/tsmc-founder-morris-chang-offered-top-job-jensen-huang-memoir-shows-2024-11-29/",
+        "https://www.reuters.com/technology/meta-faces-trial-october-unfair-competition-case-lodged-by-spanish-media-2024-11-29/",
+        "https://www.reuters.com/sustainability/boards-policy-regulation/major-canadian-news-media-companies-launch-legal-action-against-openai-2024-11-29/",
+        "https://www.reuters.com/world/asia-pacific/australian-social-media-ban-started-with-call-act-by-poiliticans-wife-2024-11-29/"
+      ]
+    }
+  },
+  random: {
+    title: ["Here are some more options for you"],
+    options: ["YouTube", "Netflix", "Amazon Prime", "Hulu"],
+    url: {
+      link: ["https://www.youtube.com", "https://www.netflix.com/browse", "https://www.amazon.com", "https://www.hulu.com/hub/home"]
+    }
   }
-},
-  random:{
-      title: ["Here are some more options for you"],
-      options: ["YouTube","Netflix","Amazon Prime","Hulu"],
-      url: {
-          link:["https://www.youtube.com","https://www.netflix.com/browse","https://www.amazon.com","https://www.hulu.com/hub/home"]
-      }
-    }
-  };
-
-
-
-
-
+};
 
 document.getElementById("init").addEventListener("click", showChatBot);
 var cbot = document.getElementById("chat-box");
 
 var len1 = data.chatinit.title.length;
 
-//click start chat btn to open chat box
-//click close chat btn to close chat box
 function showChatBot() {
   console.log(this.innerText);
   if (this.innerText == "START CHAT") {
@@ -74,7 +73,6 @@ function showChatBot() {
 }
 
 function initChat() {
-  j = 0;
   cbot.innerHTML = "";
   for (var i = 0; i < len1; i++) {
     setTimeout(handleChat, i * 500);
@@ -86,12 +84,11 @@ function initChat() {
 
 var j = 0;
 function handleChat() {
-  console.log(j);
   var elm = document.createElement("p");
   elm.innerHTML = data.chatinit.title[j];
   elm.setAttribute("class", "msg");
   cbot.appendChild(elm);
-  j++;
+  j = (j + 1) % len1;
   handleScroll();
 }
 
@@ -108,22 +105,26 @@ function showOptions(options) {
 }
 
 function handleOpt() {
-  console.log(this);
   var str = this.innerText;
-  var textArr = str.split(" ");
-  var findText = textArr[0];
+  var findText = str.trim();
+  
   document.querySelectorAll(".opt").forEach((el) => {
     el.remove();
-  })
+  });
+  
   var elm = document.createElement("p");
   elm.setAttribute("class", "test");
-  var sp = '<span class="rep">'+findText+'</span>';
+  var sp = '<span class="rep">' + findText + '</span>';
   elm.innerHTML = sp;
   cbot.appendChild(elm);
 
-  console.log(findText.toLowerCase());
   var tempObj = data[findText.toLowerCase()];
-  handleResults(tempObj.title, tempObj.options, tempObj.url);
+  
+  if (tempObj) {
+    handleResults(tempObj.title, tempObj.options, tempObj.url);
+  } else {
+    console.error("No data found for:", findText);
+  }
 }
 
 function handleResults(title, options, url) {
@@ -138,12 +139,9 @@ function handleResults(title, options, url) {
     return JSON.stringify(url) === "{}";
   }
 
-  if (isObjectEmpty(url) == true) {
-    console.log("having more options");
+  if (isObjectEmpty(url)) {
     showOptions(options);
-  } 
-  else {
-    console.log("end result");
+  } else {
     handleOptions(options, url);
   }
 }
@@ -156,20 +154,7 @@ function handleOptions(options, url) {
     opt.setAttribute("class", "opt");
     cbot.appendChild(opt);
   }
-  // var opt = document.createElement("span");
-  //   var inp = '<a class="m-link" href="'+url.more+'">'+'See more</a>';
-  // var opt = document.createElement("span");
-  //   var inp = '<a class="m-link" href="'+url.more+'"></a>';
-
-    const isObjectEmpty = (url) => {
-      return JSON.stringify(url) === "{}";
-    }
-    console.log(isObjectEmpty(url));
-    console.log(url);
-    opt.innerHTML=inp;
-    opt.setAttribute("class","opt link");
-    cbot.appendChild(opt);
-    handleScroll();
+  handleScroll();
 }
 
 function handleScroll() {
